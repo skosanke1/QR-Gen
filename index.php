49ce6b3791d8f1002df55a34a9308b4f56943538
skosanke1 <skosanke@asu.edu>
2023-09-19 14:03:03 -0600

    Add header and intiial container

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QR Code Generator</title>
     <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/9c25ec09dc.js" crossorigin="anonymous"></script>
 </head>
 <body>
+    <header>
+        <nav class="navbar">
+            <a href="index.php">
+                <div class="logo">
+                    <img src="logo.file" alt="Logo">
+                    <span>QR-Gen</span>
+                </div>
+            </a>
+            <div class="social-icons">
+            <a href="https://github.com/skosanke1" class="fa-brands fa-github fa-2xl" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://www.linkedin.com/in/stevenkosanke" class="fa-brands fa-linkedin fa-2xl" target="_blank" rel="noopener noreferrer"></a>
+            </div>
+        </nav>
+    </header>
+    <section class="container">
+            <h1>QR Code Generator</h1>
+    </section>
     <script src="scanner.js"></script>
 </body>
 </html>
